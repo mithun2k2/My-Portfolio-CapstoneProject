18bc651947054b8aa2434c0d10a7148cf2e593c2
mithun2k2 <mithun2k2eee@gmail.com>
2024-01-14 10:01:22 +0000

    My first commit on First Capstone Project

---
+++ finance_calculators.py
@@ -0,0 +1,33 @@
+import math
+print ("Investment - To calculate the amount of interest you'll earn on your investment")
+print("Bond - To calculate the amount you'll have to pay on a home loan")
+User = input(str("Enter either 'investment' or 'bond' from the menu above to proceed: ")).lower()
+
+# Investment plan one of the condition 
+if User == "investment": 
+    if True:
+        initial_amount = float(input("How much are you depositing: \nGBP"))
+        interest_rate = float(input("At which interest percentage rate: \n"))
+        interest_rate = (interest_rate/100)
+        number_of_years = float(input("How many yesrs are you planning to invest: \n"))
+        Investment_type = str(input("Choose Simple or Compound interest: \n")).lower()
+        if Investment_type == "simple":
+            "simple" == Investment_type
+            Investment_type = initial_amount*(1 + interest_rate * number_of_years)
+            total = Investment_type
+            print(f"Your interest earned over {t} years will be GBP {total: .2f}".format()) 
+        elif Investment_type:
+            Investment_type = initial_amount * math.pow((1 + interest_rate),number_of_years) 
+            total = Investment_type
+            print (f"Your interest earned over {t} years will be GBP{total:.2f}".format())
+#Bond is another conditional
+elif User == "bond":
+        if True:
+            current_value = float(input("What is the current value of the house?\nGBP")) 
+            interest_rate = float(input("At which interest rate percentage?\n" )) 
+            interest_rate = ((interest_rate / 100)/12)
+            monthly_repay = float(input("How many months you plan to repay? \n")) 
+            monthly = float(math.floor((interest_rate * current_value)/(1 - (1+interest_rate)**(-monthly_repay))))
+            print(f"Your monthly repayment will be {monthly:.2f}".format())
+else:
+    print("Please enter a valid input. Try again.")
